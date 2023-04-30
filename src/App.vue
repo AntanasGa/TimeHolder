941fb1f7050278e997c0fa20178833cdbb3911a3
AntanasGa <a.gargasas1238@gmail.com>
2023-04-30 12:47:06 +0300

    adding unavailable overlay

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <DatabaseContext>
     <template #default="{ accessable, busy }">
+      <template v-if="accessable">
       <div class="relative top-0 inset-x-0 h-1 mb-1">
         <div v-if="busy" class="w-full h-full loadbar"></div>
       </div>
@@ -8,7 +9,12 @@
       <RouterView />
       </div>
       <Navigation />
-      <div v-if="accessable" class="fixed inset-0">unavailable</div>
+      </template>
+      <div v-else class="fixed flex flex-col inset-0 bg-red-500 text-white justify-center items-center gap-2">
+        <NoSymbolIcon class="h-64 w-64"/>
+        <h1 class="text-5xl">Unavailable</h1>
+        <p>Could not load in database</p>
+      </div>
     </template>
   </DatabaseContext>
 </template>
@@ -17,4 +23,5 @@
 import { RouterView } from 'vue-router';
 import DatabaseContext from '@/components/DatabaseContext.vue';
 import Navigation from '@/components/Navigation.vue';
+import { NoSymbolIcon } from '@heroicons/vue/24/solid';
 </script>
