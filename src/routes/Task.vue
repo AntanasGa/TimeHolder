39709b4ee1f2698dbbf6f1cda201f7daadb8e5a2
AntanasGa <a.gargasas1238@gmail.com>
2023-04-30 21:49:23 +0300

    adding modal

--- src/routes/Task.vue
+++ src/routes/Task.vue
@@ -1,6 +1,19 @@
 <template>
-  {placeholder}
+  <Modal :onCancel="onCancel">
+    <div>{placeholder}</div>
+  </Modal>
 </template>
 
 <script setup lang="ts">
+import Modal from '@/components/Modal.vue';
+import { useRoute, useRouter } from 'vue-router';
+
+
+const route = useRoute();
+const router = useRouter();
+console.log(route.params);
+
+function onCancel() {
+  router.push('/tasks');  
+}
 </script>
