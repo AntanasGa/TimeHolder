ff8821ac7362805724ad1c8ec8d458f84c36a2e6
AntanasGa <a.gargasas1238@gmail.com>
2023-08-27 22:40:13 +0300

    adding import functionality

--- src/routes/Settings.vue
+++ src/routes/Settings.vue
@@ -38,11 +38,17 @@ import StyledButton from '@/components/formFunctions/StyledButton.vue';
 import { useDBCacheStore } from '@/stores/DBCacheStore';
 import { useIndexedDbStore } from '@/stores/IndexedDbStore';
 import { ArrowDownTrayIcon, ArrowUpTrayIcon } from '@heroicons/vue/24/solid';
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
 import { CacheTypes } from '@/stores/documents';
+import { DBContext } from '@/components/DatabaseContext.vue';
+import { ToastStatus, useMessagingStore } from '@/stores/MessagingStore';
 
 const cache = useDBCacheStore();
 const db = useIndexedDbStore();
+const toasts = useMessagingStore();
+
+const taskActions = inject<DBContext["task"]>("task");
+const entityActions = inject<DBContext["entity"]>("entity");
 
 const overwrite = ref(false);
 
@@ -77,9 +83,11 @@ async function onFileUpload(e: HTMLInputElement | undefined) {
   const prevBusy = !!db.busy;
   db.busy = true;
   await new Promise(async (resolve, reject) => {
-    if (e?.files?.length !== 1 || e.files?.[0].type != 'application/json') {
+    if (e?.files?.length !== 1 || e.files?.[0].type != 'application/json' || !entityActions || !taskActions) {
       return reject();
     }
+
+    const baseToast = {title: "Import"};
 
     const file = e.files[0];
     let imports: Partial<CacheTypes> | null = null;
@@ -87,21 +95,124 @@ async function onFileUpload(e: HTMLInputElement | undefined) {
     try {
       imports = JSON.parse(await file.text());
     } catch (e) {
+      toasts.addToast({
+        ...baseToast,
+        status: ToastStatus.Error,
+        message: "Converting from JSON failed",
+      });
       return reject();
     }
 
-    if (imports === null) {
+    if (!imports) {
+      toasts.addToast({
+        ...baseToast,
+        status: ToastStatus.Error,
+        message: "No data was present",
+      });
       return reject();
     }
 
     const requiredKeys: (keyof CacheTypes)[] = ['entity', 'task'];
-    if (Object.keys(imports).length !== requiredKeys.length || !Object.keys(imports).every((x) => (requiredKeys as string[]).includes(x))) {
+    if (Object.keys(imports).length !== requiredKeys.length
+      || !Object.keys(imports).every((x) => (requiredKeys as string[]).includes(x))
+    ) {
       console.log('preflight failed');
+      toasts.addToast({
+        ...baseToast,
+        status: ToastStatus.Error,
+        message: "Preflight checks have failed, check your file",
+      })
       return reject();
     }
+    db.busy = prevBusy;
 
-    console.log('oreflight success');
+    let anyFailed = false;
+    
+    let promises: Promise<void>[] = [];
+    if (overwrite.value) {
+      const entityIdList = cache.entity?.map(x => x.id) ?? [];
+      for (const id of entityIdList) {
+        promises.push(db.remove('entity', id)
+          .catch(err => {
+            console.error("remove entity", err, id);
+            anyFailed = true;
+          })
+        );
+      }
+      await Promise.all(promises);
 
+      promises = [];
+      const taskIdList = cache.task?.map(x => x.id) ?? [];
+      for (const id of taskIdList) {
+        promises.push(db.remove('task', id)
+          .catch(err => {
+            console.error("remove task", err, id);
+            anyFailed = true;
+          })
+        );
+      }
+      await Promise.all(promises);
+      promises = [];
+    } else {
+      const taskMap: [number, number][] = [];
+      let currentMax = cache.task?.reduce((max, cv) => max < cv.id ? cv.id : max, -1) ?? -1;
+      if (currentMax > -1) {
+        imports.task = imports.task?.sort((a, b) => a.id - b.id)
+          .map(x => (currentMax++, taskMap.push([x.id, currentMax]), {...x, id: currentMax}))
+          ?? [];
+        
+        currentMax = cache.entity?.reduce((max, cv) => max < cv.id ? cv.id : max, -1) ?? -1;
+        if (currentMax > -1) {
+          imports.entity = imports.entity?.sort((a, b) => a.id - b.id)
+            .map(x => (currentMax++, {...x, id: currentMax}))
+            ?? [];
+        }
+        const mapped: Record<number, number> = taskMap.reduce((acc, [k, v]) => ({...acc, [k]: v}), {});
+        imports.entity = imports.entity?.map((x) => ({...x, taskId: mapped[x.taskId] ?? -1})).filter(({taskId}) => taskId > -1) ?? [];
+      }
+    }
+
+    
+    for (const task of imports.task ?? []) {
+      promises.push(db.update('task', task)
+        .catch(err => {
+          console.error("update task", err, task);
+          anyFailed = true;
+        })
+      );
+    }
+    await Promise.all(promises);
+
+    promises = [];
+    for (const entity of imports.entity ?? []) {
+      promises.push(db.update('entity', entity)
+        .catch(err => {
+          console.error("update entity", err, entity);
+          anyFailed = true;
+        })
+      );
+    }
+    await Promise.all(promises);
+
+    cache.task = await (db.fetch('task')
+      .catch(err => {
+        console.error("fetch task", err);
+        anyFailed = true;
+        return [];
+      })
+    );
+    
+    cache.entity = await (db.fetch('entity').catch(err => {
+      console.error("fetch entity", err);
+      anyFailed = true;
+      return [];
+    }));
+
+    toasts.addToast({
+      ...baseToast,
+      status: anyFailed ? ToastStatus.Warn : ToastStatus.Success,
+      message: anyFailed ? "Some procedures failed, please check console for more details" : "All procedures completed successfully",
+    });
     resolve(undefined);
   })
   .catch(() => {})
