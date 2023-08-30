b276a8a465d2ea61eff60248fd8db8baeaecfdf0
AntanasGa <a.gargasas1238@gmail.com>
2023-08-30 22:19:43 +0300

    adding entity confirmation inside the window

--- src/routes/entities/Entity.vue
+++ src/routes/entities/Entity.vue
@@ -1,8 +1,15 @@
 <template>
-  <Modal :onCancel="onCancel">
+  <Modal :onCancel="() => (isDirty() ? (selectedMenu = MenuItem.Exit) : confirmProceed())">
     <div class="relative flex flex-col rounded-md drop-shadow-md backdrop-blur-sm bg-white dark:bg-stone-800 p-2 gap-2">
       <div v-if="isWorking" class="absolute loadbar h-1 top-0 left-0 right-0 rounded-full"></div>
-      <h1 class="font-bold text-2xl mb-2">{{ id < 0 ? "New entity" : `Edit entity ${entity.taskId.value}` }}</h1>
+      <div class="flex gap-2 justify-between">
+        <h1 class="font-bold text-2xl mb-2">{{ id < 0 ? "New entity" : `Edit entity ${entity.taskId.value}` }}</h1>
+        <Transition name="remove-entry">
+          <StyledButton v-if="id > -1" @click="() => selectedMenu = MenuItem.Delete">
+            <TrashIcon class="w-6 h-6" title="delete entry" />
+          </StyledButton>
+        </Transition>
+      </div>
       <div class="flex flex-col gap-4 mb-4">
         <div class="flex gap-2">
           <SearchableSelect name="TaskIndex"
@@ -31,26 +38,42 @@
         <StyledInput v-model="entity.comment!.value" type="area" name="Comment" title="Comment" />
       </div>
       <div class="flex gap-2">
-            <StyledButton :class="[
-              'col flex-1',
-              isDirty()
-                ? 'text-white dark:text-black bg-zinc-900 dark:bg-stone-100 hover:bg-zinc-800 dark:hover:bg-stone-300 dark:active:bg-stone-200 active:bg-zinc-700'
-                : 'text-inherit bg-zinc-50 dark:bg-stone-900 hover:bg-zinc-100 dark:hover:bg-stone-700 active:bg-zinc-200 dark:active:bg-stone-600',
-              ].join(' ')"
-              :disabled="!isDirty()"
-              @click="() => isDirty() && toInitial()"
-            >Reset</StyledButton>
-            <StyledButton :class="[
-              'col flex-1',
-              isDirty()
-                ? 'text-white bg-red-400 hover:bg-red-500 active:bg-red-600 dark:bg-red-800 dark:hover:bg-red-600 dark:active:bg-red-700'
-                : 'text-inherit bg-zinc-50 dark:bg-stone-900 hover:bg-zinc-100 dark:hover:bg-stone-700 active:bg-zinc-200 dark:active:bg-stone-600',
-              ].join(' ')"
-              :disabled="!isDirty()"
-              @click="() => isDirty() && onSave()"
-            >Save</StyledButton>
-          </div>
+          <StyledButton :class="[
+            'col flex-1',
+            isDirty()
+              ? 'text-white dark:text-black bg-zinc-900 dark:bg-stone-100 hover:bg-zinc-800 dark:hover:bg-stone-300 dark:active:bg-stone-200 active:bg-zinc-700'
+              : 'text-inherit bg-zinc-50 dark:bg-stone-900 hover:bg-zinc-100 dark:hover:bg-stone-700 active:bg-zinc-200 dark:active:bg-stone-600',
+            ].join(' ')"
+            :disabled="!isDirty()"
+            @click="() => isDirty() && toInitial()"
+          >Reset</StyledButton>
+          <StyledButton :class="[
+            'col flex-1',
+            isDirty()
+              ? 'text-white bg-red-400 hover:bg-red-500 active:bg-red-600 dark:bg-red-800 dark:hover:bg-red-600 dark:active:bg-red-700'
+              : 'text-inherit bg-zinc-50 dark:bg-stone-900 hover:bg-zinc-100 dark:hover:bg-stone-700 active:bg-zinc-200 dark:active:bg-stone-600',
+            ].join(' ')"
+            :disabled="!isDirty()"
+            @click="() => isDirty() && onSave()"
+          >Save</StyledButton>
+        </div>
     </div>
+    <Modal v-if="selectedMenu !== MenuItem.None" :onCancel="(() => (selectedMenu = MenuItem.None))">
+      <div class="relative flex flex-col rounded-md drop-shadow-md backdrop-blur-sm bg-white dark:bg-stone-800 p-2 gap-2">
+        <h1 class="font-bold text-xl mb-2">
+          <template v-if="selectedMenu === MenuItem.Exit">There are unsaved changes, are you sure you want to leave</template>
+          <template v-if="selectedMenu === MenuItem.Delete">Are you sure you want to remove this entry</template>
+        </h1>
+        <div class="flex gap-2">
+          <StyledButton class="col flex-1 text-white dark:text-black bg-zinc-900 dark:bg-stone-100 hover:bg-zinc-800 dark:hover:bg-stone-300 dark:active:bg-stone-200 active:bg-zinc-700"
+            @click="() => selectedMenu = MenuItem.None"
+          >No</StyledButton>
+          <StyledButton class="col flex-1 text-white bg-red-400 hover:bg-red-500 active:bg-red-600 dark:bg-red-800 dark:hover:bg-red-600 dark:active:bg-red-700"
+            @click="confirmProceed"
+          >Yes</StyledButton>
+        </div>
+      </div>
+    </Modal>
   </Modal>
 </template>
 
@@ -64,7 +87,7 @@ import { useDBCacheStore } from '@/stores/DBCacheStore';
 import { IEntity, IndexedItem } from '@/stores/documents';
 import { clearSeconds, useDateInputs } from '@/util/DateInputs';
 import { FormResponse, RefObject, useForm } from '@/util/Form';
-import { LinkIcon } from '@heroicons/vue/24/solid';
+import { LinkIcon, TrashIcon } from '@heroicons/vue/24/solid';
 import { WritableComputedRef, inject, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -80,6 +103,14 @@ const id = ref("id" in route.params ? +route.params.id : -1);
 type newOrExistingEntity = IEntity | Omit<IEntity, keyof IndexedItem>
 
 const isWorking = ref(false);
+
+enum MenuItem {
+  None = 0,
+  Exit = 1,
+  Delete = 2,
+}
+
+const selectedMenu = ref<MenuItem>(MenuItem.None);
 
 let entity: RefObject<newOrExistingEntity>;
 let isDirty: FormResponse<newOrExistingEntity>["isDirty"];
@@ -108,12 +139,12 @@ const initializeForm = (entityItem: newOrExistingEntity) => {
   [endDateDate, endDateTime] = useDateInputs(entity.endTime!, false);
 }
 
-function onCancel() {
-  const confirm = isDirty() ? window.confirm("There are unsaved changes, are you sure you want to leave") : true;
-  if (!confirm) {
-    return;
+function confirmProceed() {
+  if (selectedMenu.value === MenuItem.Delete && id.value > -1 && "id" in entity) {
+    entityActions?.remove(entity.id.value);
   }
-  router.push('/entities');  
+  selectedMenu.value = MenuItem.None;
+  router.push('/entities');
 }
 
 function onSave() {
@@ -149,10 +180,23 @@ if (id.value < 0) {
 } else {
   const tmpTask = cache.entity?.find((x) => x.id === id.value);
   if (!tmpTask) {
-    onCancel();
+    confirmProceed();
   } else {
     initializeForm(tmpTask);
 
   }
 }
 </script>
+
+<style>
+.remove-entry-leave-active,
+.remove-entry-enter-active {
+  transition: all 0.10s ease-in;
+}
+
+.remove-entry-leave-to,
+.remove-entry-enter-from {
+  opacity: 0;
+  transform: translateX(2em);
+}
+</style>
