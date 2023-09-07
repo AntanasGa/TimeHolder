993e0bdc584063c1f5a99ed9c5e6aa7dce7da589
AntanasGa <a.gargasas1238@gmail.com>
2023-09-07 22:41:35 +0300

    implementing search

--- src/routes/Index.vue
+++ src/routes/Index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="sticky flex top-0 w-full z-[1] bg-white/50 dark:bg-stone-900 dark:bg-gradient-to-t dark:from-stone-900 dark:to-white/10 backdrop-blur-md transition-all">
-    <div class="container mx-auto mb-10 mt-5">
+  <div class="sticky flex flex-col top-0 w-full z-[1] bg-white/50 dark:bg-stone-900 dark:bg-gradient-to-t dark:from-stone-900 dark:to-white/10 backdrop-blur-md transition-all">
+    <div class="flex flex-col container mx-auto mb-10 mt-5 gap-4">
       <h1 class="font-bold text-6xl">Overview</h1>
+      <div>
+        <StyledButton :class="[
+            'relative flex items-center gap-1 z-10',
+            showFilters
+              ? 'bg-red-200 dark:bg-red-700 hover:bg-red-300 dark:hover:bg-red-600'
+              : 'bg-zinc-100 dark:bg-stone-900 hover:bg-zinc-200 dark:hover:bg-stone-700'
+          ].join(' ')"
+          @click="() => showFilters = !showFilters"
+        >
+          <h2 class="font-bold text-lg">Filters</h2>
+          <ChevronDownIcon :class="['w-4 h-4 transition-all', showFilters ? 'scale-y-100' : '-scale-y-100']" />
+        </StyledButton>
+      </div>
+      <div :class="[
+          'flex flex-col w-inherit bg-stone-100 dark:bg-stone-900 dark:bg-gradient-to-t dark:from-stone-800 dark:to-stone-700 rounded-md transition-all gap-2',
+          ...(showFilters ? ['px-4 pt-12 pb-4 h-full -translate-y-14'] : ['h-0 px-4 pt-0 pb-0 -translate-y-100 pointer-events-none opacity-0'])
+        ]"
+        @transitionend="(e) => (showFilters && (<HTMLDivElement>e.target).querySelector('input')?.focus())"
+        ref="filtersEl"
+      >
+        <StyledInput name="TaskSearch" title="Search" v-model="search" />
+      </div>
     </div>
   </div>
   <section class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-5">
@@ -27,18 +49,83 @@
 
 <script setup lang="ts">
 import { useDBCacheStore } from '@/stores/DBCacheStore';
-import { ArrowRightIcon, LinkIcon, PlusIcon } from '@/components/Icons/index';
-import { onUnmounted, ref, watch } from 'vue';
-import { RouterLink } from 'vue-router';
+import { ArrowRightIcon, ChevronDownIcon, LinkIcon, PlusIcon } from '@/components/Icons/index';
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+import { StyledButton, StyledInput } from '@/components/formFunctions';
 
 const cache = useDBCacheStore();
 
-const taskList = ref(cache.task);
-const stopCacheWatch = watch(cache, (state) => {
-  taskList.value = state.task;
+const route = useRoute();
+
+const filtersEl = ref<HTMLDivElement | null>(null);
+
+const showFiltersInner = ref(false);
+const showFilters = computed({
+  get() {
+    return showFiltersInner.value;
+  },
+  set(value) {
+    if (!value && search.value.trim()) {
+      return;
+    }
+    showFiltersInner.value = value;
+  }
+});
+const search = ref("");
+
+const taskList = computed(() => {
+  if (!search.value.trim()) {
+    return cache.task ?? [];
+  }
+
+  const lcSearch = search.value.toLocaleLowerCase()
+  return cache.task?.filter(x => x.taskLink.toLocaleLowerCase().includes(lcSearch) || x.taskName.toLocaleLowerCase().includes(lcSearch)) ?? [];
+});
+
+const stopRouteWatch = watch(() => route.name,
+() => {
+  if (route.name !== 'Tasks') {
+    window.removeEventListener("keydown", searchActivator);
+  } else {
+    window.addEventListener("keydown", searchActivator);
+  }
+});
+
+function searchActivator(e: KeyboardEvent) {
+  if (e.altKey || e.shiftKey || e.ctrlKey) {
+    return;
+  }
+  if (e.key === 's') {
+    if (!showFilters.value) {
+      e.preventDefault();
+      showFilters.value = true;
+      return;
+    } else {
+      const inputEl = filtersEl.value?.querySelector("input");
+      if (inputEl !== document.activeElement) {
+        e.preventDefault();
+        inputEl?.focus();
+      }
+      return;
+    }
+  }
+
+  if (showFilters.value && e.key === 'Escape') {
+    showFilters.value = false;
+    return;
+  }
+}
+
+onMounted(() => {
+  if (route.name !== 'Tasks') {
+    return;
+  }
+  window.addEventListener("keydown", searchActivator);
 });
 
 onUnmounted(() => {
-  stopCacheWatch();
+  stopRouteWatch();
+  window.removeEventListener("keydown", searchActivator);
 });
 </script>
